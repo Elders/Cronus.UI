a8494f39b54d2b2c1de7041f67aff213d5c77361
soirbg <stoyan.dobrev@eldersoss.com>
2018-06-14 15:31:59 +0300

    Forgot that line of code in lazy module

--- src/app/lazy/lazy.module.ts
+++ src/app/lazy/lazy.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 
 import { LazyComponent }   from './lazy.component'; 
 import { MultitenantModule } from '../tenants';
